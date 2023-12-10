f8ad0dc35ea29888883e69d11e6e8909d9ff7cb0
asimaranov <a.simaranov@gmail.com>
2023-12-10 07:09:39 +0530

    Added networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,39 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 import * as dotenv from "dotenv";
+import {defineChain} from "viem";
+import { viem } from "hardhat";
+
+export const x1net = defineChain({
+  id: 195,
+  name: 'X1',
+  network: 'x1',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Ether',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://testrpc.x1.tech'],
+      webSocket: ['wss://x1testws.okx.com'],
+    },
+    public: {
+      http: ['https://testrpc.x1.tech'],
+      webSocket: ['wss://x1testws.okx.com'],
+    },
+  },
+  blockExplorers: {
+    default: { name: 'Explorer', url: 'https://www.oklink.com/x1-test' },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+      blockCreated: 5882,
+    },
+  },
+})
+
 
 dotenv.config();
 
@@ -12,11 +45,30 @@ const config: HardhatUserConfig = {
       accounts:
         [process.env.PRIVATE_KEY!],
     },
-
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts:
+        [process.env.PRIVATE_KEY!],
+      chainId: 44787
+    },
+    mantleTestnet: {
+      url: "https://rpc.testnet.mantle.xyz",
+      accounts:
+        [process.env.PRIVATE_KEY!],
+      chainId: 5001
+    },
+    X1: {
+      url: "https://testrpc.x1.tech",
+      accounts: [process.env.PRIVATE_KEY!],
+      chainId: 195,
+      chains: x1net
+    },
   },
   etherscan: {
     apiKey: {
       scrollTestnet: 'abc',
+      alfajores: 'BW2YCZBG9MZUQ35ZZV67498PXEKAZ7FVQF',
+      mantleTestnet: 'aaa'
     },
     customChains: [
       {
@@ -25,6 +77,22 @@ const config: HardhatUserConfig = {
         urls: {
           apiURL: 'https://sepolia-blockscout.scroll.io/api',
           browserURL: 'https://sepolia-blockscout.scroll.io/',
+        },
+      },
+      {
+        network: "alfajores",
+        chainId: 44787,
+        urls: {
+          apiURL: "https://api-alfajores.celoscan.io/api",
+          browserURL: "https://alfajores.celoscan.io",
+        },
+      },
+      {
+        network: "mantleTestnet",
+        chainId: 5001,
+        urls: {
+          apiURL: "https://explorer.testnet.mantle.xyz/api",
+          browserURL: "https://explorer.testnet.mantle.xyz",
         },
       },
     ],
